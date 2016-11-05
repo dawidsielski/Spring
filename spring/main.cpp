08c53e6812ea058b48595961bdb717ae90581b15
Dawid Sielski <dawid.sielski@outlook.com>
2016-11-05 17:42:17 +0100

    Empty project.

---
+++ spring/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stdlib.h>
+#include <GL/glut.h>
+
+using namespace std;
+
+//Called when a key is pressed
+void handleKeypress(unsigned char key, //The key that was pressed
+	int x, int y) {    //The current mouse coordinates
+	switch (key) {
+	case 27: //Escape key
+		exit(0); //Exit the program
+	}
+}
+
+//Initializes 3D rendering
+void initRendering() {
+	//Makes 3D drawing work when something is in front of something else
+	glEnable(GL_DEPTH_TEST);
+}
+
+//Called when the window is resized
+void handleResize(int w, int h) {
+	//Tell OpenGL how to convert from coordinates to pixel values
+	glViewport(0, 0, w, h);
+
+	glMatrixMode(GL_PROJECTION); //Switch to setting the camera perspective
+
+	glLoadIdentity(); //Reset the camera
+	gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
+}
+void drawScene() {
+
+	//Clear information from last draw
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
+
+
+	glutSwapBuffers(); //Send the 3D scene to the screen
+}
+
+void update(int value)
+{
+
+	glutPostRedisplay();
+	glutTimerFunc(35, update, 0);
+}
+
+int main(int argc, char** argv) {
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 800); //Set the window size
+
+								  //Create the window
+	glutCreateWindow("String");
+	initRendering(); //Initialize rendering
+
+					 //Set handler functions for drawing, keypresses, and window resizes
+
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+
+	glutTimerFunc(25, update, 0);
+
+	glutMainLoop(); //Start the main loop.  glutMainLoop doesn't return.
+	return 0; //This line is never reached
+}
